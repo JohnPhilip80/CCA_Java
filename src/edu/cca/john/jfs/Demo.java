5d4708f269f083e6254410bccf7a7805791b5f94
demo <john@careercraftacademy.com>
2026-06-22 15:25:55 +0530

    25. Private, Public, Getters, Setters, toString, static method, instance method

--- src/edu/cca/john/jfs/Demo.java
+++ src/edu/cca/john/jfs/Demo.java
@@ -1,4 +1,4 @@
-//Classes And Objects
+//Private, Public, Getters, Setters, toString, static method, instance method
 package edu.cca.john.jfs;
 
 import edu.cca.john.jfs.model.Mobile;
@@ -8,32 +8,38 @@ public class Demo {
 		Mobile m1 = new Mobile();
 		Mobile m2 = new Mobile();
 		Mobile m3 = new Mobile();
+		Mobile m4 = new Mobile();
 		
-		m1.id = 2001L;
-		m1.brand = "Apple";
-		m1.model = "IPhone 16 Pro Max";
-		m1.yearLaunched = 2026;
-		m1.price = 125700.00;
+		m1.setId(2001L);
+		m1.setBrand("Apple");
+		m1.setModel("IPhone 16 Pro Max");
+		m1.setYearLaunched(2026);
+		m1.setPrice(125700.00);
 		
-		m2.id = 2002L;
-		m2.brand = "Samsung";
-		m2.model = "S25";
-		m2.yearLaunched = 2025;
-		m2.price = 92800.00;
+		m2.setId(2002L);
+		m2.setBrand("Samsung");
+		m2.setModel("S25");
+		m2.setYearLaunched(2025);
+		m2.setPrice(92800.00);
 		
-		m3.id = 2003L;
-		m3.brand = "Motorola";
-		m3.model = "Edge 5";
-		m3.yearLaunched = 2023;
-		m3.price = 15900.00;
+		m3.setId(2003L);
+		m3.setBrand("Motorola");
+		m3.setModel("Edge 5");
+		m3.setYearLaunched(2023);
+		m3.setPrice(15900.00);
 		
-		//System.out.println(m1.id + " -- " + m1.brand + " -- " + m1.model + " -- " + m1.yearLaunched + " -- " + m1.price);
-		//System.out.println(m2.id + " -- " + m2.brand + " -- " + m2.model + " -- " + m2.yearLaunched + " -- " + m2.price);
-		//System.out.println(m3.id + " -- " + m3.brand + " -- " + m3.model + " -- " + m3.yearLaunched + " -- " + m3.price);
+		m4.scanMobile();
 		
-		System.out.printf("\n%-10s%-20s%-25s%-15s%15s","Id","Brand","Model","Year Launched","Price");
-		System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m1.id,m1.brand,m1.model,m1.yearLaunched,m1.price);
-		System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m2.id,m2.brand,m2.model,m2.yearLaunched,m2.price);
-		System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m3.id,m3.brand,m3.model,m3.yearLaunched,m3.price);
+		//System.out.printf("\n%-10s%-20s%-25s%-15s%15s","Id","Brand","Model","Year Launched","Price");
+		//System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m1.getId(),m1.getBrand(),m1.getModel(),m1.getYearLaunched(),m1.getPrice());
+		//System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m2.getId(),m2.getBrand(),m2.getModel(),m2.getYearLaunched(),m2.getPrice());
+		//System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m3.getId(),m3.getBrand(),m3.getModel(),m3.getYearLaunched(),m3.getPrice());
+		//System.out.printf("\n%-10d%-20s%-25s%-15d%15.2f",m4.getId(),m4.getBrand(),m4.getModel(),m4.getYearLaunched(),m4.getPrice());		
+		
+		Mobile.showHeading();
+		System.out.println(m1);
+		System.out.println(m2);
+		System.out.println(m3);
+		System.out.println(m4);
 	}
 }
--- src/edu/cca/john/jfs/model/Mobile.java
+++ src/edu/cca/john/jfs/model/Mobile.java
@@ -1,9 +1,62 @@
 package edu.cca.john.jfs.model;
 
+import java.util.Scanner;
+
 public class Mobile {
-	public Long id;
-	public String brand;
-	public String model;
-	public Integer yearLaunched;
-	public Double price;
+	private Long id;
+	private String brand;
+	private String model;
+	private Integer yearLaunched;
+	private Double price;
+	
+	public static void showHeading() {
+		System.out.printf("\n%-10s%-20s%-25s%-15s%15s\n","Id","Brand","Model","Year Launched","Price");
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public Integer getYearLaunched() {
+		return yearLaunched;
+	}
+	public void setYearLaunched(Integer yearLaunched) {
+		this.yearLaunched = yearLaunched;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return String.format("%-10d%-20s%-25s%-15d%15.2f", this.id,this.brand,this.model,this.yearLaunched,this.price);
+	}
+	public void scanMobile() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Mobile Id:");
+		this.id = scan.nextLong();scan.nextLine();
+		System.out.println("Enter Mobile Brand:");
+		this.brand = scan.nextLine();
+		System.out.println("Enter Mobile Model:");
+		this.model = scan.nextLine();
+		System.out.println("Enter Year Launched:");
+		this.yearLaunched = scan.nextInt();scan.nextLine();
+		System.out.println("Enter Mobile Price:");
+		this.price = scan.nextDouble();scan.nextLine();
+	}
 }
