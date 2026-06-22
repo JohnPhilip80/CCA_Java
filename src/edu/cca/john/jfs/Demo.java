585b9bd579996ab897d9ad29440c28154c3af0bf
demo <john@careercraftacademy.com>
2026-06-22 14:19:36 +0530

    8. Ternary Operators

--- src/edu/cca/john/jfs/Demo.java
+++ src/edu/cca/john/jfs/Demo.java
@@ -1,25 +1,14 @@
+//Ternary Operators
 package edu.cca.john.jfs;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		/*
-		++ (Increment): Increases value by 1.
-		-- (Decrement): Decreases value by 1
-		Post Increment & Post Decrement
-		Pre Increment & Pre Decrement
-		*/
-		int count = 10;
-		/*count++; // count is now 11 (count = count + 1 / count += 1)
-		count--; // count goes back to 10
-		System.out.println("Unary Operators");
-		System.out.println("Count = " + count);
-		*/
-		int answer = 0;
-		
-		answer = count-- + --count;
-		
-		System.out.println("Count = " + count);
-		System.out.println("Answer = " + answer);
+		// (condition part)?(true part):(false part)
+		// condition ? expression_if_true : expression_if_false;
+		int age = 10;
+		String status = (age >= 18) ? "Adult" : "Minor"; 
+		System.out.println("Ternary Operators:");
+		System.out.println(status); // Prints "Adult"
 	}
 }
