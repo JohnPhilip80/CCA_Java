e111141860f0d722afaa7821d4b7f1389bd291aa
demo <john@careercraftacademy.com>
2026-06-22 14:38:57 +0530

    19. 2D Array

--- src/edu/cca/john/jfs/Demo.java
+++ src/edu/cca/john/jfs/Demo.java
@@ -1,24 +1,29 @@
-//Swap Numbers
+//2D Array
 package edu.cca.john.jfs;
 
 import java.util.Scanner;
 
 public class Demo {
 	public static void main(String[] args) {
-		int a=8;
-		int b=3;
-		
-		System.out.println("Before Swapping: a=" + a + ",b=" + b);
-		
-		/*int t =0;
-		t=a;//t=8
-		a=b;//a=3
-		b=t;//b=8
-		*/
-		a=a*b;// a= 11
-		b=a/b;// b= 8
-		a=a/b;// a= 3
-		
-		System.out.println("After Swapping: a=" + a + ",b=" + b);
+		/*
+		 * 2D array is organized as matrices which can be represented as the collection of rows and column.
+		 * It is possible to define an array with more than one dimension.
+		 * multidimensional array is accessed by specifying an index for each dimension
+		 * Datatype variable_name [ ] [ ] ;
+		 */
+		//Two Dimension array in Java
+        int a[][] = {
+                {10, 20, 30, 9},
+                {40, 50, 60, 8},
+                {70, 80, 90, 7}
+        };
+        a[1][2] = 100;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(" "+a[i][j]);
+            }
+            System.out.println("");
+        }
 	}
 }
