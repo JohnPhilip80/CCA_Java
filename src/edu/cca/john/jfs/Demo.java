85efde61efc3ca12caa8df84fba0e4dec0d39f11
demo <john@careercraftacademy.com>
2026-06-22 14:15:45 +0530

    6. Logical Operators

--- src/edu/cca/john/jfs/Demo.java
+++ src/edu/cca/john/jfs/Demo.java
@@ -4,19 +4,16 @@ public class Demo {
 
 	public static void main(String[] args) {
 		/*
-		== (Equal to)
-		!= (Not equal to)
-		> (Greater than)
-		< (Less than)
-		>= (Greater than or equal to)
-		<= (Less than or equal to)
+		&& (Logical AND): Returns true if both conditions are true.
+		|| (Logical OR): Returns true if at least one condition is true.
+		! (Logical NOT): Reverses the boolean value
 		*/
-		int x = 10;
-		int y = 20;
+		boolean eligibleForVote = true;
+		boolean haveVoterId = false;
 		
-		System.out.println("Relational Operators");
-		System.out.println(x == y); // false
-		System.out.println(x != y); // true
-		System.out.println(x < y);  // true
+		System.out.println("Logical Operators");
+		System.out.println(eligibleForVote && haveVoterId); // false
+		System.out.println(eligibleForVote || haveVoterId); // true
+		System.out.println(!haveVoterId);           // true
 	}
 }
