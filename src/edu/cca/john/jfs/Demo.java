f620e26369f2dba4604b90e307e9ebda817e97d6
demo <john@careercraftacademy.com>
2026-06-22 14:25:17 +0530

    11. Math Operations

--- src/edu/cca/john/jfs/Demo.java
+++ src/edu/cca/john/jfs/Demo.java
@@ -1,22 +1,16 @@
-//Typecasting 
+//Math Operations 
 package edu.cca.john.jfs;
 
 public class Demo {
-
 	public static void main(String[] args) {
-		/*int myInt = 9;
-        // Automatic casting: int to double
-        double myDouble = myInt; 
-
-        System.out.println(myInt);      // Outputs: 9
-        System.out.println(myDouble);   // Outputs: 9.0
-        */
-		
-        double myDouble = 9.78;
-        // Manual casting: double to int
-        int myInt = (int)myDouble; 
-
-        System.out.println(myDouble);   // Outputs: 9.78
-        System.out.println(myInt);      // Outputs: 9 (decimal is lost)
+		System.out.println("Minimum Value: " + Math.min(10,3));
+		System.out.println("Maximum Value: " + Math.max(10,3));
+		System.out.println("Square Root: " + Math.sqrt(121));
+		System.out.println("Absolute Value: " + Math.abs(-10.23));
+		System.out.println("Power Of: " + Math.pow(2,8));
+		System.out.println("Round Number: " + Math.round(4.5));
+		System.out.println("Ceil Number: " + Math.ceil(4.1));
+		System.out.println("Floor Number: " + Math.floor(4.9));
+		System.out.printf("%-20s%-10.2f","Random Value: " ,Math.random() * 101);
 	}
 }
